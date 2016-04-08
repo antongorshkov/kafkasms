6529d47898663108f9be940412d9d34ca387b504
Cloud User <ec2-user@ip-172-31-16-223.ec2.internal>
2016-04-07 22:33:48 -0400

    Initial commit

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from kafka import KafkaProducer
+
+producer = KafkaProducer()
+
+def send(msg):
+    producer.send('sms', msg.encode('utf-8'))
+    return '<p>Sent %s!</p>\n' % msg
+
+application = Flask(__name__)
+application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text', 'NOTEXT'))))
+
+if __name__ == "__main__":
+    application.run(host= '0.0.0.0')
