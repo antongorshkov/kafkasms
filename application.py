65c62b030ecf28daaee5cb079c6cd14c217089de
antongorshkov <antong@gmail.com>
2016-04-07 22:37:22 -0400

    Updated message

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ producer = KafkaProducer()
 
 def send(msg):
     producer.send('sms', msg.encode('utf-8'))
-    return '<p>Sent %s!</p>\n' % msg
+    return '<p>Sent %s to sms topic</p>\n' % msg
 
 application = Flask(__name__)
 application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text', 'NOTEXT'))))
