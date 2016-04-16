a3872f0531bd05048e9de88f9b86fe0c9d1729c5
antongorshkov <antong@gmail.com>
2016-04-16 14:17:28 -0400

    Add a limit

--- redirect.py
+++ redirect.py
@@ -1,8 +1,14 @@
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 from datetime import datetime, timedelta
+import thread
+import time
 
-producer = KafkaProducer()
+producer = KafkaProducer(batch_size=128000)
+
+def send_msgs(msg, size):
+    for _ in range(size):
+        producer.send('simple1', msg.encode('utf-8'))
 
 while True:
     consumer = KafkaConsumer('sms')
@@ -10,10 +16,10 @@ while True:
         tokens = msg.value.split()
         try:
             size = int(tokens[1])
+            size = min([size,10000])
             value = tokens[0]
             new_msg = '{"value":"'+value+'", "debug":"'+msg.value+'"}'
-            for _ in range(size):
-                producer.send('simple1', new_msg.encode('utf-8'))
+            thread.start_new_thread(send_msgs, (new_msg, size))
         except:
             print "Unexpected error"
             raise
