79bc307d7f83eee84786f422d67f9ca8743d91a1
antongorshkov <antong@gmail.com>
2016-04-19 23:05:35 -0400

    remove NOTEXT parameter, not applicable

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ def send(msg):
     return '<p>Sent %s to sms topic</p>\n' % msg
 
 application = Flask(__name__)
-application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text', 'NOTEXT'))))
+application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text'))))
 
 if __name__ == "__main__":
     application.run(host= '0.0.0.0')
