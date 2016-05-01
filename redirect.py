7f63561480cc60c4e90ad537502e150bee8b92b9
antongorshkov <antong@gmail.com>
2016-05-01 10:30:59 -0400

    only send valid fruits

--- redirect.py
+++ redirect.py
@@ -5,10 +5,12 @@ import thread
 import time
 
 producer = KafkaProducer(batch_size=128000)
+with open('fruits') as f:
+  d = dict([x.rstrip(),1] for x in f)
 
 def send_msgs(msg, size):
     for _ in range(size):
-        producer.send('simple1', msg.encode('utf-8'))
+        producer.send('kafka1', msg.encode('utf-8'))
 
 while True:
     consumer = KafkaConsumer('sms')
@@ -17,9 +19,9 @@ while True:
         try:
             size = int(tokens[1])
             size = min([size,10000])
-            value = tokens[0]
-            new_msg = '{"value":"'+value+'", "debug":"'+msg.value+'"}'
-            thread.start_new_thread(send_msgs, (new_msg, size))
+            value = tokens[0].title()
+            if value in d:
+                new_msg = '{"value":"'+value+'", "debug":"'+msg.value+'"}'
+                thread.start_new_thread(send_msgs, (new_msg, size))
         except:
             print "Unexpected error"
-            raise
