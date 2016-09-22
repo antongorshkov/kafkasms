48e6a5ffecb9147f21d3204e7c6e37261986926e
Anton Gorshkov <antong@gmail.com>
2016-09-21 21:22:10 -0400

    source KAFKA Broker from env variable + capture sending number

--- application.py
+++ application.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask import request
 from kafka import KafkaProducer
+import os
 
-producer = KafkaProducer()
+producer = KafkaProducer(bootstrap_servers=os.environ["KAFKA"])
 
-def send(msg):
+def send(msg,sender):
+    msg=msg+' '+sender
     producer.send('sms', msg.encode('utf-8'))
     return '<p>Sent %s to sms topic</p>\n' % msg
 
 application = Flask(__name__)
-application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text'))))
+application.add_url_rule('/<username>', 'send', (lambda username: send(request.args.get('text'), request.args.get('msisdn'))))
 
 if __name__ == "__main__":
     application.run(host= '0.0.0.0')
