10716b987e972d8aab668ffbe1f2d66eb66469ec
Anton Gorshkov <antong@gmail.com>
2016-09-28 20:05:48 -0400

    significantly refactor / move sms to another process

--- redirect.py
+++ redirect.py
@@ -4,40 +4,57 @@ from datetime import datetime, timedelta
 import thread
 import time
 import os
-import nexmo
+import json
 
 broker = os.environ["KAFKA"]
 api_key=os.environ["API_KEY"]
 api_secret=os.environ["API_SECRET"]
 from_num=os.environ["FROM_NUM"]
-client = nexmo.Client(key=api_key, secret=api_secret)
 
-producer = KafkaProducer(bootstrap_servers=broker, batch_size=128000)
+producer = KafkaProducer(bootstrap_servers=broker, batch_size=128000, acks=0, max_in_flight_requests_per_connection=10)
+
 with open('fruits') as f:
   d = dict([x.rstrip(),1] for x in f)
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def send_json(msg,topic):
+    r = producer.send(topic, json.dumps(msg).encode('ascii'))
 
 def send_msgs(msg, size):
     for _ in range(size):
         producer.send('kafka1', msg.encode('utf-8'))
 
-while True:
-    consumer = KafkaConsumer(bootstrap_servers=broker)
-    consumer.subscribe(['sms'])
-    for msg in consumer:
-        tokens = msg.value.split()
-        try:
-            size = int(tokens[1])
-            size = min([size,10000])
-            value = tokens[0].title()
-            sender = tokens[2]
-            if value in d:
-                new_msg = '{"value":"'+value+'", "from_num":"'+sender+'", "debug":"'+msg.value+'"}'
-                response = 'Congrats! You just sent ' + tokens[1] + ' ' + value + '!'
-                r=client.send_message({'from': from_num, 'to': sender, 'text': response})
-                thread.start_new_thread(send_msgs, (new_msg, size))
-            else:
-                response = 'Sorry, I dont recognize ' + value + ' as a valid fruit.'
-                client.send_message({'from': from_num, 'to': sender, 'text': response})
+def process_tokens(tokens):
+    size = int(tokens[1])
+    size = min([size,1000])
+    value = tokens[0].title()
+    sender = tokens[2]
+    if value in d:
+        new_msg = '{"value":"'+value+'", "from_num":"'+sender+'", "debug":"'+msg.value+'"}'
+        response = 'Congrats! You just sent ' + str(size) + ' ' + value + '!'
+        send_json({'from': from_num, 'to': sender, 'text': response}, 'sms_response')
+        thread.start_new_thread(send_msgs, (new_msg, size))
+    else:
+        response = 'Sorry, I dont recognize ' + value + ' as a valid fruit.'
+        send_json({'from': from_num, 'to': sender, 'text': response}, 'sms_response')
 
+consumer = KafkaConsumer(bootstrap_servers=broker)
+consumer.subscribe(['sms'])
+
+while True:
+    for msg in consumer:
+        try:
+            tokens = msg.value.split()
+            if is_number(tokens[0]):
+                send_json({'sender': tokens[1], 'val': tokens[0]},'vote')
+                #producer.send('vote',tokens[0].encode('utf-8'))
+            else:
+                process_tokens(tokens)
         except:
             print "Unexpected error"
