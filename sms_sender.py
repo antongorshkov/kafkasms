be915a11ebd0d9c4e8a0a52b1bdcc7ca2abfbfb1
Anton Gorshkov <antong@gmail.com>
2016-09-28 20:07:09 -0400

    Change topic + add exception handling

--- sms_sender.py
+++ sms_sender.py
@@ -7,10 +7,13 @@ client      = nexmo.Client( key=os.environ["API_KEY"],
                             secret=os.environ["API_SECRET"])
 consumer    = KafkaConsumer(bootstrap_servers=os.environ["KAFKA"],
                             value_deserializer=lambda m: json.loads(m.decode('ascii')))
-consumer.subscribe(['kafka_test1'])
+consumer.subscribe(['sms_response'])
 
 while True:
     for message in consumer:
-        client.send_message({   'from'  : message.value['from'],
-                                'to'    : message.value['to'],
-                                'text'  : message.value['text']})
+        try:
+            client.send_message({   'from'  : message.value['from'],
+                                    'to'    : message.value['to'],
+                                    'text'  : message.value['text']})
+        except:
+            print 'Unexpected error'
